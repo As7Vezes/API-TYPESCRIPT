94232ba162e085f7dd8416edd27491a23eb043f3
Denys <denysn60@gmail.com>
2022-07-12 23:14:22 -0300

    rota para listar os items

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,19 +1,19 @@
 import { Router, Request, Response, NextFunction } from 'express'
+import itemsRouter from './items.routes'
 
 const routes = Router()
 
-function logRoutes(req: Request, res: Response, next: NextFunction){
-    const { method, baseUrl } = req
 
-    const response = `[${method.toUpperCase()}] ${baseUrl}`
-
-    console.log(response)
-
-    return next()
-}
-
-routes.get('/', logRoutes, (req: Request, res: Response) => {
-    return res.json({ message: 'Olá dev!'})
-})
+routes.use('/items', itemsRouter)
 
 export default routes
+
+// function logRoutes(req: Request, res: Response, next: NextFunction){
+//     const { method, baseUrl } = req
+
+//     const response = `[${method.toUpperCase()}] ${baseUrl}`
+
+//     console.log(response)
+
+//     return next()
+// }
